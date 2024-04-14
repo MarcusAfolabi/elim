b520c3378952598735d77b848eb21b3a00013c3b
MarcusAfolabi <biormarcus@gmail.com>
2024-04-14 21:42:16 +0100

    updated

--- app/Livewire/Pastors.php
+++ app/Livewire/Pastors.php
@@ -10,8 +10,8 @@ class Pastors extends Component
     public function mount()
     {
         $this->pastors = [
-                ['name' => 'Our Pastor', 'img' => '/images/pastors/rccg-elim-pastor.png', 'title' => 'Pastor'],
-                ['name' => 'Our Pastor', 'img' => '/images/pastors/rccg-elim-pastor2.png', 'title' => 'Pastor'],
+                // ['name' => 'Our Pastor', 'img' => '/images/pastors/rccg-elim-pastor.png', 'title' => 'Pastor'],
+                // ['name' => 'Our Pastor', 'img' => '/images/pastors/rccg-elim-pastor2.png', 'title' => 'Pastor'],
                 ['name' => 'Our Pastor', 'img' => '/images/pastors/rccg-elim-pastor3.png', 'title' => 'Pastor'],
                 ['name' => 'PST Ben Babafemi Ayanda', 'img' => '/images/pastors/rccg-elim-senior-pastor.png', 'title' => 'Senior Pastor'],
         ];
